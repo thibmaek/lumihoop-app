a86a0a1bfc084b0d3dd12d3fd5e82d0ec797be56
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-08 18:18:28 +0100

    :art: Added codemarks and check for kinect code

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofSetWindowShape(1024, 768);
+  // MARK: - Initialise socket connection
   isConnected = false;
   address = "http://localhost:3000";
   status = "not connected";
@@ -10,78 +10,91 @@ void ofApp::setup(){
   ofAddListener(socketIO.notifyEvent, this, &ofApp::gotEvent);
   ofAddListener(socketIO.connectionEvent, this, &ofApp::onConnection);
   
+  // MARK: - Initialise Kinect connection
   kinect.init();
   kinect.open();
-  kinect.isConnected() ? ofLog() << "kinect connected" : ofLog() << "kinect not connected";
-//
-//  easyCam.enableMouseInput();
-//  pointCloud.setMode(OF_PRIMITIVE_POINTS);
+  if (kinect.isConnected()) {
+    easyCam.enableMouseInput();
+    pointCloud.setMode(OF_PRIMITIVE_POINTS);
+  }
   
+  // MARK: - Preset drawing setting
+  ofSetWindowShape(1024, 768);
   ofSetCircleResolution(64);
   ofSetFrameRate(60);
 }
 
 void ofApp::update() {
-//  kinect.update();
-//  if(kinect.isFrameNewDepth()) {
-//    pointCloud.clear();
-//    for(int y = 0; y < kinect.height; y++) {
-//      for(int x= 0; x < kinect.width; x++) {
-//        int z = kinect.getDistanceAt(x, y);
-//        if(z > 0) {
-//          pointCloud.addColor(ofColor(0xff));
-//          ofVec3f pt = kinect.getWorldCoordinateAt(x, y);
-//          pointCloud.addVertex(pt);
-//        }
-//      }
-//    }
-//  }
+  // MARK: - Generate pointcloud from Kinect data
+  if(kinect.isConnected()) {
+    kinect.update();
+    if(kinect.isFrameNewDepth()) {
+      pointCloud.clear();
+      for(int y = 0; y < kinect.height; y++) {
+        for(int x= 0; x < kinect.width; x++) {
+          if(kinect.getDistanceAt(x, y) > 0) {
+            pointCloud.addColor(ofColor(0xff));
+            ofVec3f pt = kinect.getWorldCoordinateAt(x, y);
+            pointCloud.addVertex(pt);
+          }
+        }
+      }
+    }
+  }
 }
 
 void ofApp::draw() {
   ofBackground(0);
   // ofDrawBitmapStringHighlight(ofApp::status, 20, 20);
   
+  // MARK: - Draw hoop if global vars are not null
   if(hoopX && hoopY && hoopScale) {
     ofSetColor(255, 105, 180);
     ofNoFill();
     ofSetLineWidth(5);
     ofDrawCircle(hoopX, hoopY, hoopScale * 100);
   }
-//  easyCam.begin();
-//  glPushMatrix();
-//  ofScale(1, -1, -1);
-//  ofTranslate(0, 0, -100);
-//  ofEnableDepthTest();
-//  pointCloud.drawVertices();
-//  ofDisableDepthTest();
-//  ofPopMatrix();
-//  easyCam.end();
+  
+  // MARK: - Display the pointcloud & start easyCam
+  easyCam.begin();
+  glPushMatrix();
+  ofScale(1, -1, -1);
+  ofTranslate(0, 0, -100);
+  ofEnableDepthTest();
+  pointCloud.drawVertices();
+  ofDisableDepthTest();
+  ofPopMatrix();
+  easyCam.end();
 }
 
+// MARK: - #BIND_ON_CONNECT
 void ofApp::onConnection () {
   isConnected = true;
   bindEvents();
 }
 
+// MARK: - #BIND_EVENTS_TO_CALLBACKS
 void ofApp::bindEvents () {
   std::string hoopPlacedEventName = "drawHoop";
   socketIO.bindEvent(hoopPlacedEvent, hoopPlacedEventName);
   ofAddListener(hoopPlacedEvent, this, &ofApp::drawHoop);
 }
 
+// MARK: - #PROPAGATE_EVENT
 void ofApp::gotEvent(string& name) {
   ofLogNotice("ofxSocketIO[gotEvent]", name);
   status = name;
 }
 
 //--------------------------------------------------------------
-// event callbacks
+// MARK: - #EVENTS
 void ofApp::drawHoop (ofxSocketIOData& data) {
+  // MARK: - Log data to console
   ofLogNotice("ofxSocketIO[pageX]", ofToString(data.getIntValue("pageX")));
   ofLogNotice("ofxSocketIO[pageY]", ofToString(data.getIntValue("pageY")));
   ofLogNotice("ofxSocketIO[scale]", ofToString(data.getFloatValue("scale")));
   
+  // MARK: - Assign data values to global variables
   hoopX = data.getIntValue("pageX");
   hoopY = data.getIntValue("pageY");
   hoopScale = data.getFloatValue("scale");
